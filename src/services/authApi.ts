f0ad455e0f0eaf9853df14130137a5f8f97c2d91
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2025-12-05 10:34:39 +0100

    endpoint-fix

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -249,7 +249,7 @@ export interface GetEventsQueryArgs {
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/api/v1/',
+        baseUrl: 'https://eduexcel-backend.onrender.com/api/v1/',
         prepareHeaders: (headers, { getState }) => {
             const token = (getState() as RootState).auth.token;
             if (token) {
